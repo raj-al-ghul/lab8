d43e33468bac47e282089ddfdd8dd4a87f4168e5
Dilraj Singh <dilrajsingh.7@gmail.com>
2016-02-25 11:42:51 -0800

    lab8 done
    fb + gMaps + camera

---
+++ public/js/facebook.js
@@ -0,0 +1,43 @@
+
+window.fbAsyncInit = function() {
+FB.init({
+  appId      : '181254178916689',
+  xfbml      : true,
+  version    : 'v2.5'
+});
+};
+
+(function(d, s, id){
+ var js, fjs = d.getElementsByTagName(s)[0];
+ if (d.getElementById(id)) {return;}
+ js = d.createElement(s); js.id = id;
+ js.src = "//connect.facebook.net/en_US/sdk.js";
+ fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+
+function checkLoginState() {
+  FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+  });
+}
+
+function statusChangeCallback(response) {
+  console.log('Facebook login status changed.');
+  console.log(response);
+  // The response object is returned with a status field that lets the
+  // app know the current login status of the person.
+  // Full docs on the response object can be found in the documentation
+  // for FB.getLoginStatus().
+  if (response.status === 'connected') {
+    // Logged into your app and Facebook.
+    FB.api('/me?fields=name,first_name,picture.width(480)', changeUser);
+  }
+}
+
+function changeUser(response) {
+	console.log(response);
+	$('#name').html(response.name);
+	$('#photo').attr("src", response.picture.data.url);
+}
+
---
+++ public/js/googleMaps.js
@@ -0,0 +1,16 @@
+function initMap() {
+  // Create center marker at CICC 32.8849865,-117.2413314
+  var cicc_ltlng = {lat:32.8849865, lng:-117.2413314};
+
+  // Create a map object and specify the DOM element for display.
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: cicc_ltlng,
+    zoom: 18 // old -15
+  });
+
+  var marker = new google.maps.Marker({
+      position: cicc_ltlng,
+      map: map,
+      title: 'CICC'
+  });
+}
